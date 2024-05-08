1d161e81dc66bcd024ea8495dc88576a87d7a16e
araya_a <arayaanderson@gmail.com>
2024-05-08 17:13:44 -0500

    Added if else statements

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-var showCatButton = document.querySelector(".show-cat");
-console.log(showCatButton.outterHTML);
+var button = document.querySelector(".show-cat");
+console.log(button.outterHTML);
 
 var cat = document.querySelector(".cat");
 console.log(cat.outerHTML);
 
-showCatButton.addEventListener("click", function () {
-  cat.classList.add("show");
+button.addEventListener("click", function () {
+  if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("dissapear");
+  }
+  //console.log("Yes");
+  else {
+    cat.classList.add("show");
+    button.innerText = "Shoo, cat!!";
+    button.classList.remove("disappear");
+    //console.log("No");
+  }
 });
